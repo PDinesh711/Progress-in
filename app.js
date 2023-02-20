026fed6fbcf0495307ad74ee43a18ac6afdc0df0
PDinesh711 <110612494+PDinesh711@users.noreply.github.com>
2023-02-20 20:05:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var f_name , l_name , intial , bool;
+f_name="Dinesh"; l_name="Kumar";intial='P' ; bool=true;
+
+var  u_name="Dinesh";
+var password ="123@dinesh";
+
+// var i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+var someData =[10,20,30,50,40];
+var someData1 =[60,70,90]
+var newData = someData.join().concat(","+someData1);
+console.log(newData);
